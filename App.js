294a1d93ea08602c17a9e1b00234a4fa12f351ea
davideoliveira <davidomingos816@gmail.com>
2022-09-10 03:16:41 -0300

    Criado func getWatson e responseWatson

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { MainStackNavigator } from './navigation/StackNavigators';
+import ChatBotScreen from './screens/ChatBotScreen';
 
 const App = () => {
   return (
-   <NavigationContainer>
-     <MainStackNavigator/>
-   </NavigationContainer>
+  //  <NavigationContainer>
+  //    <MainStackNavigator/>
+  //  </NavigationContainer>
+      <ChatBotScreen/>
   );
 }
 
--- screens/ChatBotScreen.js
+++ screens/ChatBotScreen.js
@@ -2,16 +2,47 @@ import React, {useState, useCallback, useEffect} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
 
 
-export default function App() {
+export default function ChatBotScreen() {
   // Onde armazena as mensagens
   const [messages, setMessages] = useState([]);
+  const [session_id, setSession_id] = useState('')
+
+  const getWatson = async (props) => {
+    try {
+        const response = await fetch(`https://healthappchatbot.herokuapp.com/conversa?mensagem=${props}&session_id=${session_id}`)
+        const json = await response.json()
+        return json
+
+    } catch (error) {
+        console.log(error)
+    }  
+  }
+
+  const responseWatson = async (props) => {
+    const resposta = await getWatson(props)
+    console.log(resposta)
+    setSession_id(resposta.session_id)
+    console.log('session id', session_id)
+    onSend([
+        {
+            _id: new Date(),
+            text: resposta.text,
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+            },
+          }
+    ])
+
+
+  }
 
   // Onde vai ser feito o armazenamento das mensagens
   useEffect(() => {
     setMessages([
       {
         _id: 1,
-        text: 'Hello developer',
+        text: 'Olá! Eu sou a assistente virtual do HealthApp! \nComo posso ajuda-lo?',
         createdAt: new Date(),
         user: {
           _id: 2,
@@ -20,9 +51,10 @@ export default function App() {
         },
       },
     ]);
+    
   }, []);
 
-  console.log(messages);
+  
 
   // Callback quando estiver enviando a mensagem
   const onSend = useCallback((messages = []) => {
@@ -34,7 +66,7 @@ export default function App() {
   return (
     <GiftedChat
       messages={messages}
-      onSend={(messages) => onSend(messages)}
+      onSend={(messages) =>{onSend(messages), console.log(messages[0].text), responseWatson(messages[0].text)}}
       user={{
         _id: 1,
       }}
